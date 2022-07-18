4fabb7b38c894ad0b58dea1cfd5e47191b04da7b
Chiran <adhikari3035@gmail.com>
2022-07-18 13:01:16 -0400

    Added more files and designed using CSS.

--- index.js
+++ index.js
@@ -1,49 +1,80 @@
+// Initializing all elements Constants
+
 const temperatureField = document.querySelector(".weather1")
 const cityField = document.querySelector(".weather2 p")
 const dateField = document.querySelector(".weather2 span")
 const emojiField = document.querySelector(".weather3 img")
 const weatherField = document.querySelector(".weather3 span")
-// const searchField = document.querySelector(".searchField")
-// const form = document.querySelector("form")
+const searchField = document.querySelector(".search-Field")
+const form = document.querySelector("form")
 
 
-// Fetching Data from weather api
+// Default LOCATION
 
-let target = "kentucky,USA"
+let target = "new york, usa"
 
-const fetchData = async () => {
+// Function to fetch DATA from Weather API
 
-  const url = `https://api.weatherapi.com/v1/current.json?key=43dbb2ddf4b44abbb78170858221507&q=${target}`
+const fetchData = async (target) => {
+  try {
+    const url = `https://api.weatherapi.com/v1/current.json?key=43dbb2ddf4b44abbb78170858221507&q=   ${target}`
 
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log(data);
+    const response = await fetch(url);
+    const data = await response.json();
 
-  const {
-    current: { temp_f, condition: { text, icon }, },
-    location: { name, localtime },
-  } = data;
+    // Destructuring 
+    const {
+      current: { temp_f, condition: { text, icon }, },
+      location: { name, localtime },
+    } = data;
 
-  updateDom(temp_f, name, localtime, icon, text);
+    // Calling update DOM Function
+    updateDom(temp_f, name, localtime, icon, text);
+
+  } catch (error) {
+    alert("Location Not Found!")
+  }
 
 };
+
+// Function to update the DOM
 
 function updateDom(temperature, city, time, emoji, text) {
   temperatureField.innerText = temperature
   cityField.innerText = city
-  const exactTime = time.split(" ")[1]
-  const exactDate = time.split(" ")[0]
 
+  const exactTime = time.split(" ")[1]
+  // console.log(exactTime)
+  const exactDate = time.split(" ")[0]
   const exactDay = new Date(exactDate).getDay()
+
+
 
   dateField.innerText = `${exactTime} - ${getDayFullName(exactDay)} ${exactDate}`
   emojiField.src = emoji
   weatherField.innerText = text
+  console.log(exactDay)
 
 
 }
 
-fetchData();
+fetchData(target);
+
+
+//Function to search for Location
+
+function search(e) {
+  e.preventDefault();
+
+  target = searchField.value;
+  fetchData(target);
+};
+
+// Adding event Listener to the form
+
+form.addEventListener("submit", search);
+
+// Function to get name of the day
 
 function getDayFullName(num) {
   switch (num) {
